1e0caeaa5c69307edd43d99e0fce60e5d67f7e38
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2026-03-18 14:03:17 +0400

    tests: log_test.py

---
+++ tests/api_tests/log_test.py
@@ -0,0 +1,61 @@
+from pprint import pprint
+import allure
+import pytest
+from pages.rest_api_methods import RESTApi
+from tests.constants import Urls, Tokens, Headers, IntegrationsConstants, StatusCodes
+from pages.assertions import Assertions
+
+
+@allure.epic('LogsTest')
+class TestApi:
+    url = Urls
+    token = Tokens
+    page = RESTApi()
+    header = Headers
+    const = IntegrationsConstants
+    code = StatusCodes
+
+
+@allure.suite('Logs')
+@pytest.mark.manual_run
+class TestLog(TestApi):
+
+    @allure.title('Получить логи активности пользователей')
+    def test_get_logs_activity(self):
+        url = f'{self.url.MAIN_URL}/internal-api/logs/activity'
+        headers = self.header.HEADERS_ADMIN
+        response = self.page.get(url, headers=headers)
+        # pprint(response.json())
+        Assertions.assert_status_code(response, self.code.OK)
+
+    @allure.title('Получить список всех типов событий')
+    def test_get_logs_event_types(self):
+        url = f'{self.url.MAIN_URL}/internal-api/logs/event-types'
+        headers = self.header.HEADERS_ADMIN
+        response = self.page.get(url, headers=headers)
+        pprint(response.json())
+        Assertions.assert_status_code(response, self.code.OK)
+
+    @allure.title('генерировать Excel отчёт по логам активности')
+    def test_get_logs_activity_report(self):
+        url = f'{self.url.MAIN_URL}/internal-api/logs/activity-report'
+        headers = self.header.HEADERS_ADMIN
+        response = self.page.get(url, headers=headers)
+        # pprint(response.json())
+        Assertions.assert_status_code(response, self.code.OK)
+
+    @allure.title('Получить логи cron задач')
+    def test_get_logs_cron(self):
+        url = f'{self.url.MAIN_URL}/internal-api/logs/cron?name=LadaAcademiaIntegration'
+        headers = self.header.HEADERS_ADMIN
+        response = self.page.get(url, headers=headers)
+        # pprint(response.json())
+        Assertions.assert_status_code(response, self.code.OK)
+
+    @allure.title('Получить список уникальных имен cron задач')
+    def test_get_logs_cron_names(self):
+        url = f'{self.url.MAIN_URL}/internal-api/logs/cron/names'
+        headers = self.header.HEADERS_ADMIN
+        response = self.page.get(url, headers=headers)
+        # pprint(response.json())
+        Assertions.assert_status_code(response, self.code.OK)
